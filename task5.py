22abb77f61845d4584652659a7f9572f6d9420a6
KseniiaAlekseeva <csushaalekseeva@gmail.com>
2023-04-09 19:11:32 +0300

    Add task5

---
+++ task5.py
@@ -0,0 +1,7 @@
+vitya_num = int(input())
+fact_num = int(input())
+
+if vitya_num != fact_num:
+    print(vitya_num+fact_num-1)
+else:
+    print('Couldn\'t be calculated without additional information')
